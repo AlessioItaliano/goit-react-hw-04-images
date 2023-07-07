44e54657784b2441029780454f3c4b5d471faf91
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2023-07-07 17:09:18 +0300

    fixed big img

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,3 +1,50 @@
+// import { createPortal } from 'react-dom';
+// import { useEffect } from 'react';
+// import PropTypes from 'prop-types';
+
+// import { Backdrop, ModalField, LargeImg } from './Modal.styled';
+
+// const ModalRoot = document.getElementById('modal-root');
+
+// const Modal = ({ image, onClose }) => {
+//   useEffect(() => {
+//     const keyDown = e => {
+//       if (e.code === 'Escape') {
+//         onClose();
+//       }
+//     };
+//     window.addEventListener('keydown', this.keyDown);
+
+//     return () => {
+//       window.removeEventListener('keydown', keyDown);
+//     };
+//   }, [onClose]);
+
+//   const handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       onClose();
+//     }
+//   };
+
+//   const { largeImageURL } = image;
+
+//   return createPortal(
+//     <Backdrop onClick={handleBackdropClick}>
+//       <ModalField>
+//         <LargeImg src={largeImageURL} alt="img" />
+//       </ModalField>
+//     </Backdrop>,
+//     ModalRoot
+//   );
+// };
+
+// Modal.propTypes = {
+//   image: PropTypes.object.isRequired,
+//   onClose: PropTypes.func.isRequired,
+// };
+
+// export default Modal;
+
 import { createPortal } from 'react-dom';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
@@ -8,15 +55,15 @@ const ModalRoot = document.getElementById('modal-root');
 
 const Modal = ({ image, onClose }) => {
   useEffect(() => {
-    const keyDown = e => {
+    const handleKeyDown = e => {
       if (e.code === 'Escape') {
         onClose();
       }
     };
-    window.addEventListener('keydown', this.keyDown);
+    window.addEventListener('keydown', handleKeyDown);
 
     return () => {
-      window.removeEventListener('keydown', keyDown);
+      window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
 
